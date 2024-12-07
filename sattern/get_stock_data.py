4556e5ef1ef78bb985b8df05a1bd7546081ce686
sdstevenson <sdstevenson@gmail.com>
2024-12-07 18:18:35 -0500

    feat: store historical data as a json

--- sattern/get_stock_data.py
+++ sattern/get_stock_data.py
@@ -1,5 +1,37 @@
-def main():
-    print("Hello world from stock_api.py")
+import yfinance as yf
+import json
+import pprint
+import os
+from pathlib import Path
+import pandas as pd
 
-if __name__ == "__main__":
-    main()
+"""get_stock_data.py
+
+Interaction with the stock data api. Will return requested stock data as a json object. 
+
+Abstracted away to enable use of different api's, the only requirement being this returns a standardized output
+regardless of the API. 
+"""
+
+def store_history_data(ticker: str = "AAPL", period: str = "1mo", save_to_file: bool = False):
+    data = yf.Ticker(ticker)
+    historical_data = data.history(period=period)
+
+    historical_data.index = historical_data.index.strftime('%Y-%m-%d')
+    filtered_data = historical_data[['Open', 'High', 'Low', 'Close']].to_dict(orient='index')
+
+    print(filtered_data)
+
+    if save_to_file:
+        with open(f'{Path("./sattern/data")}/{ticker}_{period}_history_data.json', 'w') as file:
+            json.dump(filtered_data, file, indent=4)
+
+def load_data_from_file(ticker: str = "AAPL", period: str = "1mo", type: str = "history", file_path: str = None):
+    if (not file_path or not os.path.exists(file_path)):
+        file_path = f'{Path("./sattern/data")}/{ticker}_{period}_{type}_data.json'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            history = json.load(file)
+        return(history)
+    else:
+        print(f"File {file_path} does not exist.")
--- sattern/sattern.py
+++ sattern/sattern.py
@@ -1,8 +1,10 @@
 import sattern.get_stock_data as get_stock_data
+import json
 
 def main():
     print("Hello World from sattern.py")
-    get_stock_data.main()
+    # get_stock_data.store_history_data(save_to_file=True)
+    print(json.dumps(get_stock_data.load_data_from_file(), indent=1))
 
 
 if __name__ == "__main__":
