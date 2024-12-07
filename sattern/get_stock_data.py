289c5abaf069db011bde62cb8e5c61729ae6d84b
sdstevenson <sdstevenson@gmail.com>
2024-12-06 21:08:09 -0500

    init: set up poetry

---
+++ sattern/get_stock_data.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello world from stock_api.py")
+
+if __name__ == "__main__":
+    main()
---
+++ sattern/sattern.py
@@ -0,0 +1,9 @@
+import sattern.get_stock_data as get_stock_data
+
+def main():
+    print("Hello World from sattern.py")
+    get_stock_data.main()
+
+
+if __name__ == "__main__":
+    main()
