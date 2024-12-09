d7697fbc8092a67c99e130bf7829c62e61b622b8
sdstevenson <sdstevenson@gmail.com>
2024-12-09 16:39:04 -0500

    feat: highlight current period red

--- sattern/display_stock_data.py
+++ sattern/display_stock_data.py
@@ -2,16 +2,19 @@ from matplotlib import pyplot
 import matplotlib.dates as mdates
 from datetime import datetime
 from sattern.get_stock_data import history_data
+from typing import List
 
 """display_stock_data.py
 
-All stock visualization and graphing is done here. 
-"""
+All stock visualization and graphing is done here."""
 
-def highlight_pattern(data: history_data, start_index: int, end_index: int):
+def highlight_pattern(data: history_data, start_indicies: List[int], end_indicies: List[int]):
     fig, ax = display_stock_price(data=data, show=False)
 
-    ax.axvspan(start_index, end_index, color="blue", alpha=0.3)
+    for start, end in zip(start_indicies, end_indicies):
+        color = "red" if end == end_indicies[-1] else "blue"
+        ax.axvspan(start, end, color=color, alpha=0.3)
+
     pyplot.show()
 
 
@@ -25,8 +28,14 @@ def display_stock_price(data: history_data, show: bool = True):
     # Plot the data
     ax.plot(x_values, data.close)
     
-    # Optionally set custom x-tick labels to display dates at specific intervals
-    tick_positions = x_values[::5]  # Adjust the step as needed
+    # Adjust the x-tick labels to display dates at specific intervals
+    if (data.period == "1mo"):
+        tick_positions = x_values[::10]
+    elif (data.period == "1y"):
+        tick_positions = x_values[::100]
+    else:
+        tick_positions = x_values[::500]
+
     tick_labels = [datetime.fromtimestamp(int(data.date[i])).strftime('%m-%d') for i in tick_positions]
     ax.set_xticks(tick_positions)
     ax.set_xticklabels(tick_labels, rotation=45, ha='right')
@@ -34,7 +43,7 @@ def display_stock_price(data: history_data, show: bool = True):
     # Set labels and title
     ax.set_xlabel('Data Points')
     ax.set_ylabel('Close Value')
-    ax.set_title(f'Stock Price Plot ({data.period})')
+    ax.set_title(f'{data.ticker} Stock Price Plot ({data.period})')
     
     # Adjust layout to prevent label cutoff
     fig.tight_layout()
