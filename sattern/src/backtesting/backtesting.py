f31cd2661dbf73f25d6dee292122a2c50bbd3f23
sdstevenson <sdstevenson@gmail.com>
2024-12-21 21:48:50 +0700

    test: load multiple stocks

--- sattern/src/backtesting/backtesting.py
+++ sattern/src/backtesting/backtesting.py
@@ -3,8 +3,10 @@
 This module serves as the entry point for the backtesting functionality in the Sattern project.
 """
 
+from sattern.src.backtesting.load_data import load_stock_data
+
 def main():
-    pass
+    load_stock_data(["INTC", "GOOG", "CELH"], 2)
 
 if __name__ == "__main__":
     main()
