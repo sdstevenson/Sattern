81913e78dfb8dfc1d96795c1c954ff0224042645
sdstevenson <sdstevenson@gmail.com>
2025-01-05 17:25:03 +0100

    refactor: clean up

--- sattern/src/main.py
+++ sattern/src/main.py
@@ -1,26 +1,17 @@
 import sattern.src.tools.api as api 
 from sattern.src.metrics.sattern import sattern
-import sattern.src.tools.weekday as weekday
 from sattern.src.tools.display import display
-from typing import Dict, List
-from datetime import datetime
-import pandas as pd
 
-def run_sattern(portfolio: Dict):
-    ticker="TSM"
-    max_diff = 2
+def run_sattern(ticker: str = "ERJ"):
+    max_diff = 5
     financial_metrics = api.get_financial_metrics(ticker=ticker)
-    sattern_df = sattern(financial_metrics=financial_metrics, max_diff=max_diff)
-    financial_metrics = pd.concat([financial_metrics, sattern_df], axis=1)
-    # print(financial_metrics)
+    financial_metrics, decision = sattern(financial_metrics=financial_metrics, max_diff=max_diff)
+    # print(f"{financial_metrics}\n***{decision}***")
     display(data=financial_metrics, metrics_to_plot=["prices", "sattern", "sattern_highlight"], ticker=ticker, max_diff=max_diff)
+    return financial_metrics, decision
 
 def main():
-    portfolio = {
-        "cash": 100000.0,
-        "stock": 0
-    }
-    run_sattern(portfolio)
+    run_sattern(ticker="ERJ")
 
 if __name__ == "__main__":
     main()
