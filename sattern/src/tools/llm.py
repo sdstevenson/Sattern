21d1a6a29344ddc55459983f813a2857144e0faf
sdstevenson <sdstevenson@gmail.com>
2025-01-09 11:22:36 -0500

    feat: working llm response and filtering

--- sattern/src/tools/llm.py
+++ sattern/src/tools/llm.py
@@ -2,13 +2,14 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from typing import List, Dict
+import json
+from pathlib import Path
 
 load_dotenv()
 
-def run_llm(data):
+def run_llm(data) -> Dict:
 
-    format_rules = [
-    {
+    format_rules = [{
         "type": "function",
         "function": {
             "name": "output_prediction",
@@ -16,39 +17,35 @@ def run_llm(data):
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "prediction": {
-                        "type": "float",
-                        "description": "Price prediction of where this stock will go."
-                    },
-                    "action": {
-                        "type": "string",
-                        "description": "Recommended action based on the prediciton and confidence. One of Strong Buy, Buy, Hold, Sell, or Strong Sell"
-                    },
-                    "quantity": {
-                        "type": "int",
-                        "description": "Stock quantity to perform the given action on. Keep in mind portfolio limits."
-                    },
-                    "thought_process": {
-                        "type": "string",
-                        "description": "Thought process as you think through this."
-                    },
-                    "news": {
-                        "type": "string",
-                        "description": "Analysis of recent news and its relation to this stock."
-                    },
+                    "prediction": {"type": "number"},
+                    "action": {"type": "string"},
+                    "quantity": {"type": "integer"},
+                    "thought_process": {"type": "string"},
+                    "news": {"type": "string"},
                 },
-                "required": ["prediction", "confidence", "news"],
+                "required": ["prediction", "action", "quantity", "thought_process", "news"],
                 "additionalProperties": False,
             }
         }
-    }
-    ]
+    }]
 
     messages = [
         {
             "role": "system",
             "content": """You are a portfolio manager making financial decisions on a stock based on current news and attached metrics.
-            You will return data as specified by the `output_prediction` tool. You will keep in mind portfolio values and limits so as to not spend more than is in the portfolio."""
+            You will return data as specified by the `output_prediction` tool. You will keep in mind portfolio values and limits so as to not spend more than is in the portfolio.
+            
+            In `prediction` store a price prediction of where this stock will go. 
+
+            In `action` store a recommended action based, one of Strong Buy, Buy, Hold, Sell, or Strong Sell.
+
+            In `quantity` store the quantity of the stock to perform the given action on. Keep in mind the price of one stock.
+
+            In `thought process` write out your thought process as you fill in these fields.
+
+            In `news` write a brief analysis of recent news and its relation to this stock. 
+
+            """
         },
         {
             "role": "user",
@@ -59,7 +56,7 @@ def run_llm(data):
             This data is from the stock {data["ticker"]}. Retrieve and analyse recent news from this stock to help inform your decision. 
 
             Here are certain times where the stock price moved similarly to how it is moving now, with the data following the date the difference between these periods and the most recent period of data: {data["sattern_highlight"]}.
-            Compare these periods and the stock prive movement after to the current period. Use this to predict where this stock price will move next.
+            Compare these periods and the stock price movement after to the current period. Use this to predict where this stock price will move next.
 
             Here is what a custom formula predicts the price movement will be: {data["sattern"]}.
 
@@ -67,7 +64,7 @@ def run_llm(data):
 
             Here is the stock price over time: {data["prices"]}.
 
-            Fill out all fields of the `output_prediction` tool and return. 
+            Fill out all fields of the `output_prediction` tool and return.
             """
         }
     ]
@@ -83,14 +80,20 @@ def run_llm(data):
         temperature=0.2,
         top_p=0.7,
         max_tokens=1024,
-        stream=True,
+        stream=False,
+        # response_format=format_rules
         tools=format_rules,
-        tool_choice="output_prediction"
+        tool_choice="auto"
     )
 
-    for chunk in completion:
-        if chunk.choices[0].delta.content is not None:
-            print(chunk.choices[0].delta.content, end="")
+    response = completion.choices[0].message.tool_calls[0].function.arguments
+    parsed_response = json.loads(response)
+    print(parsed_response)
+
+    # with open(f'{Path("./sattern/src/data")}/AI_RESPONSE.json', 'w') as f:
+    #     json.dump(parsed_response, f, indent=4)
+
+    return parsed_response
 
 if __name__ == "__main__":
     messages = [{"role":"user","content":"Write a limerick about the wonders of GPU computing."}]
