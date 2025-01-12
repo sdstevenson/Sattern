3da4f0e4039c75a46713b234420911a80639eed4
sdstevenson <sdstevenson@gmail.com>
2025-01-11 20:57:16 -0500

    feat: add new data to api.py

--- sattern/src/tools/api.py
+++ sattern/src/tools/api.py
@@ -1,6 +1,6 @@
-import yfinance
 import requests
 import os
+import json
 import pandas as pd
 from pathlib import Path
 from datetime import datetime, timedelta
@@ -35,6 +35,9 @@ def get_financial_metrics(
         start_date, end_date = end_date, start_date
 
     # Collect metrics
+    news = get_news(ticker, start_date, end_date)
+    return
+
     prices_df = get_prices(ticker, start_date, end_date)
     print(prices_df)
 
@@ -69,21 +72,68 @@ def get_prices(ticker: str, start_date: datetime, end_date: datetime) -> pd.Data
 
     return df
 
-def get_recommendations(ticker: str) -> pd.DataFrame:
-    yf = yfinance.Ticker(ticker)
-    return yf.get_recommendations()
+# def get_recommendations(ticker: str) -> pd.DataFrame:
+#     yf = yfinance.Ticker(ticker)
+#     return yf.get_recommendations()
 
-def get_news(ticker: str) -> List:
-    yf = yfinance.Ticker(ticker)
-    return yf.get_news()
+def get_news(ticker: str, start_date: datetime, end_date: datetime):
+    args = {
+        "function": "NEWS_SENTIMENT",
+        "tickers": ticker,
+        "time_from": start_date.strftime("%Y%m%dT%H%M"),
+        "time_to": end_date.strftime("%Y%m%dT%H%M"),
+        "sort": "RELEVANCE",
+        "limit": "1",
+    }
+    url = construct_url(**args)
+    news = requests.get(url).json()
+    # with open(f'{Path("./sattern/src/data")}/{ticker}_news.json', 'w') as f:
+    #     json.dump(news, f)
+    return news
 
-def get_price_targets(ticker: str) -> Dict:
-    yf = yfinance.Ticker(ticker)
-    return yf.get_analyst_price_targets()
+    # Get the average sentiment, weighted by the relevance score
+    total_relevance = 0
+    total_sentiment = 0
+    count = 0
+    for article in news["feed"]:
+        for rating in news[article]["ticker_sentiment"]:
+            if rating["ticker"] == ticker:
+                relevance = float(rating["ticker"]["relevance_score"])
+                sentiment = float(rating["ticker"]["ticker_sentiment_score"])
+                total_relevance += relevance
+                total_sentiment += sentiment * relevance
+
+    weighted_sentiment = total_sentiment / total_relevance
+
+def get_insider_transactions(ticker: str, start_date: datetime, end_date: datetime):
+    args = {
+        "function": "INSIDER_TRANSACTIONS",
+        "symbol": ticker
+    }
+    url = construct_url(**args)
+    insider_transactions = requests.get(url).json()
+    data = []
+    for transaction in insider_transactions["data"]:
+        data.append(
+            {
+            "date": datetime.strptime(transaction["transaction_date"], "%Y-%m-%d"),
+            "acquisition_or_disposal": transaction["acquisition_or_disposal"],
+            "shares": transaction["shares"],
+            "share_price": transaction["share_price"]
+            }
+        )
+    df = pd.DataFrame(data)
+    df.set_index("date", inplace=True)
+    return df
+
+# def get_price_targets(ticker: str) -> Dict:
+#     yf = yfinance.Ticker(ticker)
+#     return yf.get_analyst_price_targets()
 
 def construct_url(**args):
     url = f'https://www.alphavantage.co/query?'
     for arg in args.keys():
         url += f"{arg}={args[arg]}&"
     url += f"apikey={os.getenv('STOCK_API_KEY')}"
+    print(url)
     return url
