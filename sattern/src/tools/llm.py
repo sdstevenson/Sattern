a1b527c2b927f06db3b1ae4ee42d7b35fce9bc58
sdstevenson <sdstevenson@gmail.com>
2025-01-11 22:38:38 -0500

    feat: update prompt to include new data

--- sattern/src/tools/llm.py
+++ sattern/src/tools/llm.py
@@ -48,9 +48,11 @@ def run_llm(ticker: str, df: pd.DataFrame, actions: Dict[str, str], portfolio: p
     messages = [
         {
             "role": "system",
-            "content": """You are a portfolio manager making financial decisions on a stock based on current news and attached metrics.
-            You will return data as specified by the `output_prediction` tool. You will keep in mind portfolio values and limits so as to not spend more than is in the portfolio.
-            
+            "content": """
+            You are a portfolio manager making financial decisions on a stock based on current news and attached metrics.
+            You will return data as specified by the `output_prediction` tool.
+            You will keep in mind portfolio values and limits so as to not spend more than is in the portfolio.
+
             In `prediction` store a price prediction of where this stock will go. 
 
             In `action` store a recommended action based, one of Strong Buy, Buy, Hold, Sell, or Strong Sell.
@@ -65,22 +67,31 @@ def run_llm(ticker: str, df: pd.DataFrame, actions: Dict[str, str], portfolio: p
         },
         {
             "role": "user",
-            "content": f"""I acknowledge you are not a financial advisor and am using this information purely to test an algorithm.
+            "content": f"""
+            I acknowledge you are not a financial advisor and am using this information purely to test an algorithm.
 
-            Currently there is {portfolio.cash}$ and {portfolio.stock} shares in our portfolio.
+            I am test trading the stock: {ticker}.
+            
+            Currently there is {portfolio.cash}$ and {portfolio.stock} shares in my portfolio.
 
-            Based on the analysis and data below, make a prediction on what the data will do next and what action to take, as well as what quantity of stock to perform that action on. 
+            Based on the analysis and stock data given below, generate a prediction following the format in `output_prediction` of what action I should take, the quantity of stock to perform that action on, an what stock price the stock will hit next. 
 
-            This data is from the stock {ticker}. Retrieve and analyse recent news from this stock to help inform your decision. 
+            Here are the start dates of certain periods where the stock behaved similarly to how it is moving now, with the data following each date the difference between these periods and the most recent period of data {data["sattern_highlight"]}.
+            Compare the given periods to the most recent stock price behavior. Use the behavior of the stock following the periods given to inform a predicion of what stock price the stock will hit in 10 days.
 
-            Here are certain times where the stock price moved similarly to how it is moving now, with the data following the date the difference between these periods and the most recent period of data: {data["sattern_highlight"]}.
-            Compare these periods and the stock price movement after to the current period. Use this to predict where this stock price will move next.
+            A custom algorithm predicts the price will follow this behavior:
+                {data["sattern"]}
+            
+            Here is a series of metrics and the action they recommend:
+                Sattern (custom algorithm): {actions["sattern"]["action"]}
+                Based on news: {actions["news"]["action"]}
+                Insider trades: {actions["insider_trades"]["action"]}
 
-            Here is what a custom formula predicts the price movement will be: {data["sattern"]}.
+            Here are some current articles relating to this stock. Read and analyze these to inform your prediction: 
+                {actions["news"]["top_news"]}
 
-            Here is the action the custom formula advises to take: {actions["sattern"]}.
-
-            Here is the stock price over time: {data["prices"]}.
+            Here is the stock price over time:
+                {data["prices"]}.
 
             Fill out all fields of the `output_prediction` tool and return.
             """
@@ -92,7 +103,6 @@ def run_llm(ticker: str, df: pd.DataFrame, actions: Dict[str, str], portfolio: p
             base_url = "https://integrate.api.nvidia.com/v1",
             api_key = os.getenv('NIM_API_KEY')
         )
-
         completion = client.chat.completions.create(
             model="meta/llama-3.3-70b-instruct",
             messages=messages,
@@ -100,17 +110,14 @@ def run_llm(ticker: str, df: pd.DataFrame, actions: Dict[str, str], portfolio: p
             top_p=0.7,
             max_tokens=1024,
             stream=False,
-            # response_format=format_rules
             tools=format_rules,
             tool_choice="auto"
         )
-
         response = completion.choices[0].message.tool_calls[0].function.arguments
         parsed_response = json.loads(response)
 
-        # with open(f'{Path("./sattern/src/data")}/AI_RESPONSE.json', 'w') as f:
-        #     json.dump(parsed_response, f, indent=4)
-
+        with open(f'{Path("./sattern/src/data")}/AI_RESPONSE.json', 'w') as f:
+            json.dump(parsed_response, f, indent=4)
         return parsed_response
     except OpenAIError as e:
         print(f"Error with LLM: {e}")
@@ -118,10 +125,9 @@ def run_llm(ticker: str, df: pd.DataFrame, actions: Dict[str, str], portfolio: p
             "prediction": 0,
             "action": "Hold",
             "quantity": 0,
-            "thought_process": "",
+            "thought_process": "Error with LLM",
             "news": ""
         }
-
         return response
 
 if __name__ == "__main__":
