f713448e9c7abba88f82ec8d4f605833574d3407
sdstevenson <sdstevenson@gmail.com>
2025-02-05 09:07:25 -0500

    fix: Update quantity caclulation

--- sattern/src/trader.py
+++ sattern/src/trader.py
@@ -7,39 +7,21 @@ class portfolio():
         self.stock = stock
         self.display = display
 
-    def execute_trade(self, action: Union[Dict, str], current_price: float, quantity: int = None, show: bool = False):
-        if quantity is None or type(action) == Dict:    # Quantity will only be none if action is a Dict
+    def execute_trade(self, action: str, current_price: float, quantity: int = None, show: bool = False) -> int:
+        if quantity is None:
             # Set the amounts to buy
-            strong_signal_quantity = 10000//current_price
-            normal_signal_quantity = strong_signal_quantity//2
-            num_metrics = 0
-            metric_avg = 0
-            for metric in action.keys():
-                num_metrics += 1
-                metric_action = action[metric]["action"]
-                if metric_action == "Strong Buy":
-                    metric_avg += 1
-                elif metric_action == "Buy":
-                    metric_avg += 0.5
-                elif metric_action == "Sell":
-                    metric_avg -= 0.5
-                elif metric_action == "Strong Sell":
-                    metric_avg -= 1
-            if metric_avg >= 0.8:
+            strong_signal_quantity = 10000//current_price//2
+            normal_signal_quantity = strong_signal_quantity//4
+            if action == "Strong Buy":
                 quantity = strong_signal_quantity
-                action = "Strong Buy"
-            elif metric_avg >= 0.1:
+            elif action == "Buy":
                 quantity = normal_signal_quantity
-                action = "Buy"
-            elif metric_avg > -0.1:
-                quantity = 0
-                action = "Hold"
-            elif metric_avg > -0.8:
+            elif action == "Sell":
                 quantity = -1 * normal_signal_quantity
-                action = "Sell"
-            else:
+            elif action == "Strong Sell":
                 quantity = -1 * strong_signal_quantity
-                action = "Strong Sell"
+            else:
+                quantity = 0
 
         cost = abs(quantity) * current_price
 
@@ -53,6 +35,43 @@ class portfolio():
 
         if show:
             print(f"{action} {quantity} @ {current_price}")
+        return quantity
 
     def __str__(self) -> str:
         return f"Portfolio(cash={self.cash:.2f}, stock={self.stock})"
+
+def combine_signals(actions: Dict, curr_price: float):
+    # Set the amounts to buy
+    strong_signal_quantity = 10000//curr_price
+    normal_signal_quantity = strong_signal_quantity//2
+    num_metrics = 0
+    metric_avg = 0
+
+    for metric in actions.keys():
+        num_metrics += 1
+        metric_action = actions[metric]["action"]
+        if metric_action == "Strong Buy":
+            metric_avg += 1
+        elif metric_action == "Buy":
+            metric_avg += 0.5
+        elif metric_action == "Sell":
+            metric_avg -= 0.5
+        elif metric_action == "Strong Sell":
+            metric_avg -= 1
+    if metric_avg >= 0.8:
+        quantity = strong_signal_quantity
+        action = "Strong Buy"
+    elif metric_avg >= 0.1:
+        quantity = normal_signal_quantity
+        action = "Buy"
+    elif metric_avg > -0.1:
+        quantity = 0
+        action = "Hold"
+    elif metric_avg > -0.8:
+        quantity = -1 * normal_signal_quantity
+        action = "Sell"
+    else:
+        quantity = -1 * strong_signal_quantity
+        action = "Strong Sell"
+
+    return action, quantity
